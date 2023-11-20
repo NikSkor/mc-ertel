c2fec4199e5b18934f42d1fdff7bc8f031118d69
NikSkor <mr-zerx@yandex.ru>
2023-11-20 07:36:06 +0500

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,20 @@
-const saveBtn = document.querySelector('.info__btn');
-const inputForm = document.querySelector('.info__form');
+const saveBtn = document.querySelector('.info__btn_modal');
+const formModal = document.querySelector('.info__modal');
+const backBtn = document.querySelector('.info__btn_back');
+const inputs = document.querySelectorAll('.info__input');
+
+saveBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  formModal.classList.add('info__modal_active');
+  inputs.forEach(elem => {
+    elem.readOnly = true;
+  });
+})
+
+backBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  formModal.classList.remove('info__modal_active');
+  inputs.forEach((elem) => {
+    elem.readOnly = false;
+  });
+})
